233bb1cb37ef0b74131f718eb6a68c26dbbd8525
Jiří Martínek <zwo@centrum.cz>
2021-11-05 11:14:12 +0100

    next is 5. Rozhraní (interface)

--- article_04/04_methods_ptr_better_solution.go
+++ article_04/04_methods_ptr_better_solution.go
@@ -18,17 +18,17 @@ import (
 	"math"
 )
 
-// Line je datová struktura, ke které budou přidány metody
-type Line struct {
+// Linia je datová struktura, ke které budou přidány metody
+type Linia struct {
 	x1, y1 float64
 	x2, y2 float64
 }
 
-func (line Line) length() float64 {
+func (line Linia) length() float64 {
 	return math.Hypot(line.x1-line.x2, line.y1-line.y2)
 }
 
-func (line *Line) translate(dx, dy float64) {
+func (line *Linia) translate(dx, dy float64) {
 	fmt.Printf("Translating line %v by %f %f\n", *line, dx, dy)
 	line.x1 += dx
 	line.y1 += dy
@@ -37,7 +37,7 @@ func (line *Line) translate(dx, dy float64) {
 }
 
 func main() {
-	line1 := Line{x1: 0, y1: 0, x2: 100, y2: 100}
+	line1 := Linia{x1: 0, y1: 0, x2: 100, y2: 100}
 
 	fmt.Println(line1)
 	line1.translate(5, 5)
